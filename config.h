181363e725e41e287299ea7f3ea626620d623d1a
spy <spyhoodle@gmail.com>
2022-02-23 12:40:54 +0000

    small changes

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *fonts[]          = { "Iosevka:pixelsize=14:antialias=true:aut
                                         "Font Awesome 6 Brands:pixelsize=14:antialias=true:autohint=true",
 };
 static const char dmenufont[]       = "Iosevka:pixelsize=14:antialias=true:autohint=true";
-#include "/home/spy/cols/dwm.h"
+#include "/home/spy/col/dwm.h"
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
 static const XPoint stickyiconbb    = {4,8};  /* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
 
